d06d75ca0c774b1ef104f1929f7bc51622a91add
Benjamin Lawson <lawbe101@yahoo.com>
2017-10-19 17:25:16 -0400

    initialized firebase 4.5.1

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  console.log('I get here');
 
-  console.log(moment().format('hh:mma MMM Do YYYY'))
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBVxfamITNH4qG7B5Dkri14UXuLO2fZRfk",
+    authDomain: "train-time-937d3.firebaseapp.com",
+    databaseURL: "https://train-time-937d3.firebaseio.com",
+    projectId: "train-time-937d3",
+    storageBucket: "train-time-937d3.appspot.com",
+    messagingSenderId: "385888615867"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
 })
